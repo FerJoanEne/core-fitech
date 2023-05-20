e673d7ce8fee17aefd16ad49f1e878f0c0313bf8
AgustinMacias <agus_masus@outlook.com>
2023-05-20 19:31:33 -0300

    Update Test US2

--- src/test/java/core/UserStory2.java
+++ src/test/java/core/UserStory2.java
@@ -1,22 +1,26 @@
 package core;
 
 //java
+
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import services.ValidatorFactory;
+import services.ValidatorFinder;
 
 //custom imports
 import java.io.File;
 import java.io.FileNotFoundException;
 
 
-
 public class UserStory2 {
 
     private final Logger log = LogManager.getLogger("UserStory2");
 
+    private ValidatorFinder validatorFinder;
 
     @BeforeEach
     public void setUp() {
@@ -24,62 +28,59 @@ public class UserStory2 {
     }
 
     @Test
-    public void CA1(){
+    public void CA1() {
         log.warn("se ejecuta test de CA1 - UBICACION INEXISTENTE");
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> {
-            new CoreFitech("", "Bicicleta1");
+        validatorFinder = new ValidatorFinder("");
+        assertThrows(FileNotFoundException.class, () -> {
+            validatorFinder.findValidators();
         });
-
-        log.warn("mensaje de error CA1: {} ", illegalArgumentException.getMessage());
     }
 
 
     @Test
     public void CA2() {
         log.warn("se ejecuta test de CA2 - UBICACION INVALIDA");
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> {
-            new CoreFitech("\\videos", "Bicicleta1");
+        validatorFinder = new ValidatorFinder("\\jhjgh");
+        assertThrows(IllegalArgumentException.class, () -> {
+            validatorFinder.findValidators();
         });
 
-        log.warn("mensaje de error CA2: {} ", illegalArgumentException.getMessage());
     }
 
     @Test
     public void CA3() throws FileNotFoundException {
         log.warn("se ejecuta test de CA3 - CARPETA VACIA");
-        CoreFitech coreFitech = new CoreFitech(new File("emptyFolder").getAbsolutePath(), "Bicicleta1");
-        int size = coreFitech.getValidationTask().getValidators().size();
+        String currentDirectory = System.getProperty("user.dir");
+        String path = currentDirectory + File.separator + "validatorEmpty";
+        validatorFinder = new ValidatorFinder(path);
+        int size = validatorFinder.findValidators().size();
         assertEquals(0, size);
-        log.warn("cantidad de validators: {}", size);
     }
 
     @Test
     public void CA4() throws FileNotFoundException {
         log.warn("se ejecuta test de CA4 - NO ES VALIDACION");
-        File file = new File("src/test/noEsValidacion");
-        CoreFitech coreFitech = new CoreFitech(file.getAbsolutePath(), "Bicicleta1");
-        int size = coreFitech.getValidationTask().getValidators().size();
+        String path = new File("validatorIsNot").getAbsolutePath();
+        validatorFinder = new ValidatorFinder(path);
+        int size = validatorFinder.findValidators().size();
         assertEquals(0, size);
-        log.warn("cantidad de validators: {}", size);
     }
 
     @Test
     public void CA5() throws FileNotFoundException {
         log.warn("se ejecuta test de CA5 - VALIDACION SIMPLE");
-        File file = new File("src/test/validacionSimple");
-        CoreFitech coreFitech = new CoreFitech(file.getAbsolutePath(), "Bicicleta1");
-        int size = coreFitech.getValidationTask().getValidators().size();
+        String path = new File("validator").getAbsolutePath();
+        validatorFinder = new ValidatorFinder(path);
+        int size = validatorFinder.findValidators().size();
         assertEquals(1, size);
-        log.warn("cantidad de validators: {}", size);
     }
 
     @Test
     public void CA6() throws FileNotFoundException {
         log.warn("se ejecuta test de CA6 - VALIDACIONES MULTIPLES");
-        File file = new File("src/test/validacionMultiple");
-        CoreFitech coreFitech = new CoreFitech(file.getAbsolutePath(), "Bicicleta1");
-        int size = coreFitech.getValidationTask().getValidators().size();
+        String path = new File("validators").getAbsolutePath();
+        validatorFinder = new ValidatorFinder(path);
+        int size = validatorFinder.findValidators().size();
         assertEquals(2, size);
-        log.warn("cantidad de validators: {}", size);
     }
 }
